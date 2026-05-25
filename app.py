2cfc3be5fd38f60c59bc9bf9f97a3b40e23c0dbb
mctey <tey.mingchuan@yahoo.com.sg>
2026-05-26 00:58:54 +0800

    Added general metrics for monitoring

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 from utils.supabase_utils import extract_all_rows
+from utils.statistics_utils import compute_metrics
 from config import FEEDBACK_NAME
 
 st.set_page_config(layout="wide")
 
-# Title
-st.title("⏱️ Lateness Prediction Monitoring Dashboard")
+# Title #########################################################################################
+left, center, right = st.columns([1, 6, 1])
+
+with center:
+    st.title("⏱️ Lateness Prediction Monitoring Dashboard")
+
+#################################################################################################
 
 # Refreshes every hour
 st_autorefresh(interval=3600000)
 
 feedback_df = extract_all_rows(FEEDBACK_NAME)
 
-st.dataframe(feedback_df, use_container_width=True, height=600)
+# General Metrics ###############################################################################
+metrics = compute_metrics(feedback_df)
+
+left, center, right = st.columns([1, 20, 1])
+
+with center:
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    col1.metric("MAE", f"{metrics['mae']:.2f}")
+    col2.metric("RMSE", f"{metrics['rmse']:.2f}")
+    col3.metric("Bias", f"{metrics['bias']:.2f}")
+    col4.metric("P90 Error", f"{metrics['p90_error']:.2f}")
+    col5.metric("Samples", metrics["count"])
+
+#################################################################################################
---
+++ utils/statistics_utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+def compute_metrics(df: pd.DataFrame):
+    if df.empty:
+        return {
+            "mae": 0,
+            "rmse": 0,
+            "bias": 0,
+            "p90_error": 0,
+            "count": 0
+        }
+
+    df["act_min"] = (
+        pd.to_datetime(df["arrived_time"]) -
+        pd.to_datetime(df["meeting_time"])
+    ).dt.total_seconds() / 60
+
+    errors = df["act_min"] - df["pred_min"]
+
+    metrics = {
+        "mae": np.mean(np.abs(errors)),
+        "rmse": np.sqrt(np.mean(errors ** 2)),
+        "bias": np.mean(errors),
+        "p90_error": np.percentile(np.abs(errors), 90),
+        "count": len(df)
+    }
+
+    return metrics
